3fbafc9624adea924d895994a54531b15e69008d
joel <hendersonjoel0@gmail.com>
2024-03-07 11:39:09 -0500

    created  directories and file structure for react coponents starting search coponents

---
+++ src/components/app/App.js
@@ -0,0 +1,32 @@
+import React, {useState, useCallback} from 'react';
+import './App.css';
+
+
+
+
+const App = () => {
+  // const [searchResults, setSearchResults] = useState([]);
+  // const [playlistName, setPlaylistName] = useState('New Playlist');
+  // const [playlistTracks, setPlaylistTracks] = useState([]);
+
+
+
+  return (
+    <div className="App">
+      <h1>Jamming</h1>
+      <div className='App'>
+       {/* search bar*/}
+       <div className='App-playlist'>
+        {/* search results */}
+        {/* Playlist */}
+       </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
---
+++ src/components/tracklist/tracklist.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './tracklist.css';
+
+
+
+
+
+const TrackList = () => {
+    return(
+    };
+    
+    export default TrackList;
+    
+    
+                  
+                
+            
+   
