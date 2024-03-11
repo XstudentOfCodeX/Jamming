db7a60b0d01106575855998005052e7fd406e360
joel <hendersonjoel0@gmail.com>
2024-03-11 13:37:00 -0400

    updating index.js variables adding comments

--- src/utility/SpotifyApi.js
+++ src/utility/SpotifyApi.js
@@ -1,5 +1,5 @@
-const clientId = '0cddbe26103848499a78cd2f7dfa1427';
-const clientSecret = '3fec045d65434d5b9f9fe42571e1907b';
+const clientId = '';
+const clientSecret = '';
 let accessToken; 
 
 const Spotify = {
